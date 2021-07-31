733f0d62239f8cf02d2724dcdc6e5e50fa9c99ee
Song Xue <songxue.aws@gmail.com>
2021-07-31 22:28:48 +1000

    Greeting API

---
+++ src/main/java/info/songxue/java/sample/api/rest/Greeting.java
@@ -0,0 +1,13 @@
+package info.songxue.java.sample.api.rest;
+
+public class Greeting {
+  private final String content;
+
+  public Greeting(String content) {
+    this.content = content;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
